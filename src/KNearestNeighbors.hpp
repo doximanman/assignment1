c8391519f139e6850e02fc4ff6342a2fcc34771d
doximanman <kalo39570@gmail.com>
2022-08-02 21:47:16 +0300

    removed using from hpp file

--- src/KNearestNeighbors.hpp
+++ src/KNearestNeighbors.hpp
@@ -6,21 +6,19 @@
 #define ASS1_KNEARESTNEIGHBORS_HPP
 
 #include "Distance.hpp"
-using namespace Geometry;
-
 class KNearestNeighbors {
 private:
     /**
      * vector of Point vectors where the first dimension is the data
      * type, and the second dimension is the data of that type.
      */
-    vector<vector<Point>> _data;
-    Point _modelPoint;
+    vector<vector<Geometry::Point>> _data;
+    Geometry::Point _modelPoint;
     /**
      * returns the indices of the K closest points to the model point.
      * @return
      */
-    vector<int[2]> firstK(int K, Distance &distance);
+    vector<int[2]> firstK(int K, Geometry::Distance &distance);
     /**
      * @param list
      * @param value
@@ -38,19 +36,19 @@ public:
      * @param modelPoint
      * @param data
      */
-    KNearestNeighbors(const Point& modelPoint, vector<vector<Point>> data);
+    KNearestNeighbors(const Geometry::Point& modelPoint, vector<vector<Geometry::Point>> data);
     /**
      * @param distance Distance function
      * @return a table of all the distances from the point (according to the data table -
      *         in the same order)
      */
-    vector<vector<double>> distances(Distance &distance);
+    vector<vector<double>> distances(Geometry::Distance &distance);
     /**
      * @param K
      * @param distance
      * @return K nearest neighbors, in accordance with the provided distance function.
      */
-    vector<Point> nearestNeighbors(int K, Distance &distance);
+    vector<Geometry::Point> nearestNeighbors(int K, Geometry::Distance &distance);
 
     // returns the index of the determined data type.
     /**
@@ -60,13 +58,13 @@ public:
      * @param distance
      * @return the classification.
      */
-    int classify(int K,Distance &distance);
+    int classify(int K,Geometry::Distance &distance);
     /**
      * Prints the K nearest members, according to the distance function.
      * @param K
      * @param distance
      */
-    void printNearestNeighbors(int K,Distance &distance);
+    void printNearestNeighbors(int K,Geometry::Distance &distance);
 };
 
 #endif //ASS1_KNEARESTNEIGHBORS_HPP
