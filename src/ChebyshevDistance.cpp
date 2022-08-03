810bc0823dbcf5e98c33cb94cb80bb1902e11844
doximanman <kalo39570@gmail.com>
2022-08-03 23:44:19 +0300

    removed abs function

--- src/ChebyshevDistance.cpp
+++ src/ChebyshevDistance.cpp
@@ -9,7 +9,7 @@ double ChebyshevDistance::distance(Point p1, Point p2) {
     int dimension=p1.dimension;
     double maxDifference=-1;
     for(int i=0;i<dimension;i++){
-        double difference=abs(p1.at(i)-p2.at(i));
+        double difference=Distance::absoluteValue(p1.at(i)-p2.at(i));
         if(maxDifference<difference)
             maxDifference=difference;
     }
