61373badd643276c8dc23ebd782327ca9d22421c
doximanman <kalo39570@gmail.com>
2022-08-04 11:49:52 +0300

    fixed more bugs

--- src/ChebyshevDistance.cpp
+++ src/ChebyshevDistance.cpp
@@ -3,16 +3,14 @@
 //
 
 #include "ChebyshevDistance.hpp"
+
 using namespace Geometry;
 
 double ChebyshevDistance::distance(Point p1, Point p2) {
     int dimension=p1.dimension;
     double maxDifference=-1;
     for(int i=0;i<dimension;i++){
-        double difference=p1.at(i)-p2.at(i);
-        if(difference<0){
-            difference*=-1;
-        }
+        double difference=std::abs(p1.at(i)-p2.at(i));
         if(maxDifference<difference)
             maxDifference=difference;
     }
